ed76f2dcf4cea29806dcf74e43068f9935f3de66
foshesss <foshessss@gmail.com>
2023-08-31 00:54:41 -0400

    initial push w/ docs + example db output

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # fartbot
+[https://fartclub.aspear.net](https://fartclub.aspear.net)
 
-## [https://fartclub.aspear.net](https://fartclub.aspear.net)
+## Guide to use codebase
+1. Create a virtual environment: `python -m venv venv`
+2. Activate virtual environment: `. venv/bin/activate`
+3. Install/update dependencies: `poetry install`
+4. Start the app locally: `flask --app main --debug run`
+
+## How to add new libraries
+`poetry add library@version`
+
+```python
+(
+    userid, # 0 
+    longeststreak_start_date, # 1
+    longeststreak_end_date, # 2
+    currentstreak_start_date, # 3
+    currentstreak_end_date, # 4
+    longeststreak_length, # 5
+    currentstreak_length, # 6
+    pfp,
+    name,
+    total
+) 
+```
+## Expected DB JSON Format
+```json
+[
+    [
+        "", // userid
+        "2023-01-04", // longeststreak_start_date
+        "2023-01-04", // longeststreak_end_date
+        "2023-01-04", // currentstreak_start_date
+        "2023-01-04", // currentstreak_end_date
+        0, // longesstreakt_length
+        0, // currentstreak_length
+        "", // pfp
+        "derpmonster83", // username
+        500 // total
+    ]
+]
+```
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from flask import Flask, render_template
+from pathlib import Path
+
+import json
 import sqlite3
 
 app = Flask(__name__)
 
+def file_exists(file_name: str):
+    return False
+
+DB_LOCATION = "/home/pi/projects/fartbot/fartstreak.db"
+
 @app.route("/")
 def main():
-    cur = sqlite3.connect("/home/pi/projects/fartbot/fartstreak.db").cursor()
-    res = cur.execute('SELECT * FROM fartstreak').fetchall()
+    res = None # pre-define for scope
+
+    # used for people testing without sqlite DB
+    if file_exists(DB_LOCATION) == True:
+        cur = sqlite3.connect(DB_LOCATION).cursor()
+        res = cur.execute('SELECT * FROM fartstreak').fetchall()
+
+        #print(type(out))
+        #print(type(rend))
+    else:
+        # example.json should always exist
+        f = open("example.json")
+        res = json.load(f)
+
+    # sort based on longest streak
     rend = sorted(res, key=lambda x: x[5])
     out = rend[::-1]
-    #print(type(out))
-    #print(type(rend))
-    #rend = res.sort(key = lambda x: x[5])
+
+    # see README for 'out' json format
     return render_template("main.html", Database = out)
 
 if __name__ == '__main__':
